7bba93f56499342142c77a1b04531f578db7adeb
Abraham De León <abraham.deleon@iteso.mx>
2023-11-21 23:32:03 -0600

    ConfigManager

--- AutoNMS.py
+++ AutoNMS.py
@@ -1,5 +1,6 @@
 from netmiko import ConnectHandler
-import re, csv
+from datetime import datetime
+import re, csv, os
 
 R1 = {
     'device_type': 'cisco_ios_telnet',
@@ -7,6 +8,7 @@ R1 = {
     'username': 'netadmin',
     'password': 'Passw0rd',
     'port': 23,  # Telnet port
+    'secret': 'Passw0rd',
 }
 
 R2 = {
@@ -15,6 +17,7 @@ R2 = {
     'username': 'netadmin',
     'password': 'Passw0rd',
     'port': 23,  # Telnet port
+    'secret': 'Passw0rd',
 }
 
 R3 = {
@@ -23,6 +26,7 @@ R3 = {
     'username': 'netadmin',
     'password': 'Passw0rd',
     'port': 23,  # Telnet port
+    'secret': 'Passw0rd',
 }
 
 R4 = {
@@ -31,6 +35,7 @@ R4 = {
     'username': 'netadmin',
     'password': 'Passw0rd',
     'port': 23,  # Telnet port
+    'secret': 'Passw0rd',
 }
 
 R5 = {
@@ -39,6 +44,7 @@ R5 = {
     'username': 'netadmin',
     'password': 'Passw0rd',
     'port': 23,  # Telnet port
+    'secret': 'Passw0rd',
 }
 
 devices = [R1,R2,R3,R4,R5]
@@ -104,9 +110,65 @@ class AutoNMS:
     def confManager(self):
         for device in self.devices:
             try:
-                pass
+                # Obtener el hostname del dispositivo
+                hostname = self.get_hostname(device)
+
+                # Crear una carpeta con el nombre del hostname si no existe
+                folder_path = f"config/{hostname}"
+                os.makedirs(folder_path, exist_ok=True)
+
+                with ConnectHandler(**device) as conn:
+                    # Obtener la configuración del dispositivo y guardarla en un archivo
+                    conn.enable()
+                    config_output = conn.send_command('wr t')
+
+                timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+                config_filename = f"{folder_path}/config_{timestamp}.txt"
+
+                previous_config_file = self.getLatestConfig(folder_path)
+
+                if previous_config_file:
+                    # Leer la configuración anterior
+                    with open(previous_config_file, 'r') as prev_file:
+                        previous_config = prev_file.read()
+
+                    # Comparar la configuración actual con la anterior
+                    if config_output != previous_config:
+                        # Guardar la configuración actual
+                        with open(config_filename, 'w') as config_file:
+                            config_file.write(config_output)
+                        print(f"Configuración de {hostname} ha cambiado y se ha guardado en {config_filename}")
+                    else:
+                        print(f"Configuración de {hostname} no ha cambiado, no se guarda nada.")
+                else:
+                    # No hay configuraciones anteriores, guardar la configuración actual
+                    with open(config_filename, 'w') as config_file:
+                        config_file.write(config_output)
+                    print(f"Configuración de {hostname} guardada en {config_filename}")
+
             except Exception as e:
                 print(f"Error en la conexión a {device['ip']}: {e}")
 
+    def get_hostname(self, device):
+        try:
+            with ConnectHandler(**device) as conn:
+                hostname_output = conn.send_command_timing("show whoami")
+                hostname_match = re.search(r"Comm Server \"(R\d+)\"", hostname_output)
+                if hostname_match:
+                    return hostname_match.group(1)
+        except Exception as e:
+            print(f"Error obteniendo el hostname de {device['ip']}: {e}")
+        return "Unknown"
+    
+    def getLatestConfig(self, folder_path):
+        config_files = [] 
+        elements_in_folder = os.listdir(folder_path)
+        for element in elements_in_folder:
+            if element.startswith("config_"):
+                config_files.append(element)
+        if config_files:
+            return os.path.join(folder_path, max(config_files))
+        return None
+
 auto_nms = AutoNMS(devices)
-auto_nms.saveIPAM()
+auto_nms.confManager()
